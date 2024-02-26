44f24e124dc5ececda0cd6499e07a3d3445e77b0
Rose Dias <rosedias.info@gmail.com>
2024-02-26 17:54:02 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  projectId: 'a5ac536-10c7-4ef6-89f3-a62cad78ca6b',
   e2e: {
     baseUrl: 'https://blogdoagi.com.br/',
     env: {
