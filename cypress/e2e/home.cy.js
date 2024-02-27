265ae2a64ad3cb4dc7e2f0b8b85f46781dccb853
rosedias <rosedias.info@gmail.com>
2024-02-27 15:46:35 -0300

    update

--- cypress/e2e/home.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-describe('home', () => {
-  it('webapp On', () => {
-    cy.visit('/')
-    cy.title().should('eq', 'Blog do Agi | Tudo sobre empréstimo e educação financeira')
-  })
-})
